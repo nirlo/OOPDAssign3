a48173e1fc5b0528c3c69838a651d231e0ae87c2
pl <pl@MSI>
2018-04-10 17:56:53 -0400

    Merge origin/master into Factory-Builder
    Conflicts:
    	src/dataaccess/EmployeeDAOImp.java
    	src/factory/AbstractFactory.java
    	src/factory/EmployeeFactory.java

--- src/factory/DeptEmpFactory.java
+++ src/factory/DeptEmpFactory.java
@@ -1,46 +1,47 @@
 package factory;
 
+import builder.DeptEmpBuilder;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import transfer.DeptEmployee;
 
-public class DeptEmpFactory extends AbstractFactory {
-
+public class DeptEmpFactory extends AbstractFactory<DeptEmployee> {
+	
 	/**
-	 * 
+	 *
 	 */
 	@Override
-	public <T> T createFromResultSet(ResultSet rs) {
+	public  DeptEmployee createFromResultSet(ResultSet rs) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 	
 	/**
-	 * 
+	 *
 	 */
 	@Override
-	public List<?> createListFromResultSet(ResultSet rs) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * 
-	 */
-	@Override
-	public <T> T createFromMap(Map<String, String[]> map) {
-		// TODO Auto-generated method stub
+	public DeptEmployee createFromMap(Map<String, String[]> map) {
 		return null;
 	}
 	/**
-	 * 
+	 *
+	 * @return
+	 * @throws java.sql.SQLException
 	 */
 	@Override
-	public List<?> createListResultSet(ResultSet rs) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<DeptEmployee> createListFromResultSet(ResultSet rs) throws SQLException {
+		return createListFromMap(super.resultSetToMap(rs));
 	}
-
-	
-	
+	@Override
+	public List<DeptEmployee> createListFromMap(Map<String,String[]> map) throws SQLException{
+		List<DeptEmployee> list = Collections.emptyList();
+		for (Map<String, String> m: (List<Map<String, String>>)super.createListFromMap(map)){
+			list.add(new DeptEmpBuilder(m).get());
+		}
+		return list;
+		
+	}
 }
