38255a29dcc1819c2c54052a0ef4a1d6a6881006
pl <pl@MSI>
2018-04-12 18:24:25 -0400

    12/04/18 - Created new createConnection()

--- src/dataaccess/DataSource.java
+++ src/dataaccess/DataSource.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,17 +16,28 @@ import javax.naming.NamingException;
  * @see https://tomcat.apache.org/tomcat-8.0-doc/jndi-resources-howto.html
  */
 public class DataSource {
-
-    public Connection createConnection() {
-        Connection connection = null;
-        try {
-            Context initCtx = new InitialContext();
-            Context envCtx = (Context) initCtx.lookup("java:comp/env");
-            javax.sql.DataSource ds = (javax.sql.DataSource) envCtx.lookup("jdbc/Registrar");
-            connection = ds.getConnection();
-        } catch (NamingException | SQLException ex) {
-            Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return connection;
-    }
+	
+//    public Connection createConnection() {
+//        Connection connection = null;
+//        try {
+//            Context initCtx = new InitialContext();
+//            Context envCtx = (Context) initCtx.lookup("jdbc:mysql://localhost:3306employees");
+//            javax.sql.DataSource ds = (javax.sql.DataSource) envCtx.lookup("jdbc:mysql://localhost:3306employees");
+//            connection = ds.getConnection();
+//        } catch (NamingException | SQLException ex) {
+//            Logger.getLogger(DataSource.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return connection;
+//    }
+	
+	public Connection createConnection() throws SQLException{
+		Connection connection = null;
+		try{
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees", "root", "root");
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+	}
+	return connection;
+}
 }
